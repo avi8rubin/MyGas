3dd924cf069056a10a7c6dce29c584175849e986
zinoo <zinoo@OhadZino>
2015-12-03 10:28:23 +0200

    עדכון לabstruct callback

--- MyGas/src/callback/CallBack.java
+++ MyGas/src/callback/CallBack.java
@@ -2,12 +2,14 @@ package callback;
 
 import java.io.Serializable;
 
+import common.MessageType;
+
 public abstract class CallBack implements Serializable{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	protected MessageType WhatToDo;
 	public CallBack(){};
 }
