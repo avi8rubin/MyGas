ae1e22ac251029ee3857dfabf8cc4ca7cf03e527
zinoo <zinoo@OhadZino>
2015-12-08 22:28:21 +0200

    הוספת שאילתות לCEO

--- MyGas/src/common/MessageType.java
+++ MyGas/src/common/MessageType.java
@@ -17,6 +17,9 @@ public enum MessageType {
 	getCheckExistsUserPass,
 	getContacts,
 /*Marketing Manager*/	
-	getCommentsForMarketionCampaign
+	getCommentsForMarketionCampaign,
+/*CEO*/
+	getWaitingTariff,
+	setWaitingTariff
 
 }
