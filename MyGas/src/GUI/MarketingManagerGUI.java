056f538990bf7d1264c758d4a33d84943cf89d85
ליטף <@10.100.102.5>
2015-12-12 01:16:03 +0200

    הוספתי: callbackActivateSaleCampaign callbackCustomerCharacterizationByPeriod callbackBuyHomeFuel callbackTariffUpdate callbackCommentsForMarketionCampaign

--- MyGas/src/GUI/MarketingManagerGUI.java
+++ MyGas/src/GUI/MarketingManagerGUI.java
@@ -1,8 +1,19 @@
 package GUI;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.LineBorder;
+
 import callback.callbackBuffer;
 import callback.callbackUser;
 import client.Client;
+import common.TableModel;
 
 public class MarketingManagerGUI extends abstractPanel_GUI{
 
@@ -10,11 +21,136 @@ public class MarketingManagerGUI extends abstractPanel_GUI{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	//top buttons
+	private JButton TariffButton;
+	private JButton ReportButton;
+	private JButton ActivateSaleCampaignButton;
+	//TariffButton
+	private JLayeredPane TariffDisplayLayer = new JLayeredPane();
+	private JScrollPane TariffScrollPane = new JScrollPane();
+	private JTable TariffUpdateTable = new JTable();
+	private JButton UpdateButton;
+	//ReportButton
+	private JButton CustomerCharacterizationReportButton;
+	private JButton CommentsForMarketingCampaignButton;
+	private JLayeredPane CreateReportsLeftLayer= new JLayeredPane();
+	private JLayeredPane CreateReortsCenterLayer= new JLayeredPane();
+	//
 
 	public MarketingManagerGUI(callbackUser EnteredUser, Client Server, callbackBuffer CommonBuffer,
 			Login_GUI LoginScreen) {
 		super(EnteredUser, Server, CommonBuffer, LoginScreen);
-		// TODO Auto-generated constructor stub
+		/* ------- Adding new button to Top Panel -------- */
+		TariffButton= new JButton("Tariff Update");
+		TariffButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		TariffButton.setBounds(23, 133, 160, 38);
+		TopPanel.add(TariffButton);
+		
+		/* ------- Adding new button to Top Panel -------- */
+		ReportButton = new JButton("Create Reports");
+		ReportButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		ReportButton.setBounds(211, 133, 178, 38);
+		TopPanel.add(ReportButton);
+		
+		/* ------- Adding new button to Top Panel -------- */
+		ActivateSaleCampaignButton = new JButton("Activate Sale Campaign");
+		ActivateSaleCampaignButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		ActivateSaleCampaignButton.setBounds(416, 133, 274, 38);
+		TopPanel.add(ActivateSaleCampaignButton);
+		
+
+		/* ------- Adding new layer to Tariff Panel -------- */
+		
+		TariffDisplayLayer.setBounds(0, 0, 968, 512);
+		ContactFrame.getContentPane().add(TariffDisplayLayer);
+		TariffScrollPane.setBounds(48, 92, 864, 295);
+		TariffDisplayLayer.add(TariffScrollPane);
+		TariffScrollPane.setViewportView(TariffUpdateTable);
+		TariffUpdateTable.setRowHeight(23);
+		TariffUpdateTable.setFillsViewportHeight(true);
+		TariffUpdateTable.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		TariffUpdateTable.getTableHeader().setFont(new Font("Tahoma", Font.PLAIN, 15));
+		TariffUpdateTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		
+		JLabel FuelsTariffLabel = new JLabel("Fuels Tariff");
+		FuelsTariffLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		FuelsTariffLabel.setBounds(48, 31, 147, 33);
+		TariffDisplayLayer.add(FuelsTariffLabel);
+		
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnUpdate.setBounds(787, 416, 125, 33);
+		TariffDisplayLayer.add(btnUpdate);	
+		
+		CenterCardContainer.add(TariffDisplayLayer, "Tariff");
+		TariffDisplayLayer.setOpaque(true);
+		TariffDisplayLayer.setName("Tariff");
+		
+		
+		/* ------- Adding new layer to reports Panel -------- */
+		//left layer
+		CreateReportsLeftLayer.setBounds(0, 0, 310, 544);
+		ContactFrame.getContentPane().add(CreateReportsLeftLayer);
+
+		//buttons left layer
+		CommentsForMarketingCampaignButton = new JButton
+				(String.format("%s\n%s", "Comments for ","marketing campaign"));
+		CommentsForMarketingCampaignButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		CommentsForMarketingCampaignButton.setBounds(27, 33, 212, 100);
+		
+		CustomerCharacterizationReportButton = new JButton
+				(String.format("%s\n%s","Customer characterization ","by period"));
+		CustomerCharacterizationReportButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		CustomerCharacterizationReportButton.setBounds(27, 161, 212, 100);
+		//add to container
+		LeftCardContainer.add(CreateReportsLeftLayer, "Reports Left");
+		CreateReportsLeftLayer.setOpaque(true);
+		CreateReportsLeftLayer.setName("Reports Left");
+		//center layer
+		CreateReortsCenterLayer.setBounds(0, 26, 974, 526);
+		ContactFrame.getContentPane().add(CreateReortsCenterLayer);
+		//add to container
+		CenterCardContainer.add(CreateReortsCenterLayer, "Reports Center");
+		CreateReortsCenterLayer.setOpaque(true);
+		CreateReortsCenterLayer.setName("Reports Center");
+		
+		
+		
+}
+
+	//Tariff	
+	public JButton getTariffButton(){
+		return TariffButton;
+	}
+	public JButton getUpdateButton(){
+		return UpdateButton;
+	}
+	public JLayeredPane getTariffDisplayLayer(){
+		return TariffDisplayLayer;
+	}
+	public void setTariffUpdateTable(TableModel NewTable){
+		TariffUpdateTable.setModel(NewTable);	
+	}
+	//reports
+	public JButton getReportButton(){
+		return ReportButton;
+	}
+	public JButton getCommentsForMarketingCampaignButton(){
+		return CommentsForMarketingCampaignButton;
 	}
 
+	public JButton getCustomerCharacterizationReportButton(){
+		return CustomerCharacterizationReportButton;
+	}
+	
+	//campaign sale
+	public JButton getActivateSaleCampaignButton(){
+		return ActivateSaleCampaignButton;
+	}
+
+
+
+
 }
+
+
--- MyGas/src/controller/MarketingManagerController.java
+++ MyGas/src/controller/MarketingManagerController.java
@@ -1,28 +1,117 @@
 package controller;
 
+import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
-
-import GUI.CEOGUI;
-import GUI.CustomerGUI;
+import javax.swing.JButton;
+import javax.swing.JLayeredPane;
 import GUI.MarketingManagerGUI;
 import callback.callbackBuffer;
+import callback.callbackStringArray;
 import client.Client;
+import common.MessageType;
+import common.TableModel;
 
 public class MarketingManagerController extends Controller {
 
+	/**
+	 * Marketing Manager GUI components 
+	 */
+	private CardLayout ContainerCardCenter;
+	private CardLayout ContainerCardLeft;
+//
 	private MarketingManagerGUI GuiScreen;
+	private JButton TariffButton;
+	private JButton ReportButton;
+	private JButton ActivateSaleCampaignButton;
+	private JButton UpdateButton;
+//
+	private JButton CustomerCharacterizationReportButton;
+	private JButton CommentsForMarketingCampaignButton;
+	private JLayeredPane CreateReortsCenterPanel;
+	private JLayeredPane CreateReportsLeftPanel;
+//
+	
+
 	
 	public MarketingManagerController(Client Server, callbackBuffer CommonBuffer, MarketingManagerGUI GuiScreen) {
 		super(Server, CommonBuffer, GuiScreen);
 		this.GuiScreen = GuiScreen;
 		GuiScreen.setVisible(true);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		
+		/* ------ Add action listener to buttons ------ */
+		//Tariff update Button
+		TariffButton = GuiScreen.getTariffButton();
+		TariffButton.addActionListener(this);
+		TariffButton.setActionCommand("Tariff");								//Add action command
+		
+		//Tariff Update Button
+		UpdateButton = GuiScreen.getUpdateButton();
+		UpdateButton.addActionListener(this);
+		UpdateButton.setActionCommand("Update Button");								//Add action command
+
+		//Report Button
+		ReportButton = GuiScreen.getReportButton();
+		ReportButton.addActionListener(this);
+		ReportButton.setActionCommand("Reports");								//Add action command
+
+		//Activate Sale Campaign Button
+		ActivateSaleCampaignButton = GuiScreen.getActivateSaleCampaignButton();
+		ActivateSaleCampaignButton.addActionListener(this);
+		ActivateSaleCampaignButton.setActionCommand("Activate Sale Campaign");								//Add action command
+		
+		//Customer Characterization Report
+		CustomerCharacterizationReportButton= GuiScreen.getCustomerCharacterizationReportButton();
+		CustomerCharacterizationReportButton.addActionListener(this);
+		CustomerCharacterizationReportButton.setActionCommand("Customer Characterization Report");	
+		//Comments For Marketing Campaign
+		CommentsForMarketingCampaignButton=GuiScreen.getCommentsForMarketingCampaignButton();
+		CommentsForMarketingCampaignButton.addActionListener(this);
+		CommentsForMarketingCampaignButton.setActionCommand("Comments For Marketing Campaign");	
+		//
+
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		ContainerCardCenter = (CardLayout)(CenterCardContainer.getLayout());
+		ContainerCardLeft	= (CardLayout)(LeftCardContainer.getLayout());
+		/* -------- Check the source of the event ---------*/
+		if(e.getActionCommand().equals("Tariff")){
+			ContainerCardCenter.show(CenterCardContainer, "Tariff");				//The TariffApproval layer will be display
+			HandleTariffUpdatePressed();											
+		}
+		if(e.getActionCommand().equals("Reports")){
+			ContainerCardCenter.show(CenterCardContainer,"Reports Center");
+			ContainerCardLeft.show(LeftCardContainer, "Reports Left");
+			HandleCreateReportsPressed();											
+		}
+		if(e.getActionCommand().equals("Activate Sale Campaign")){
+			HandleActivateSaleCampaignPressed();											
+		}
+		
+		if(e.getActionCommand().equals("Customer Characterization Report")){
+			HandleCustomerCharacterizationReport();											
+		}
+		if(e.getActionCommand().equals("Comments For Marketing Campaign")){
+			HandleCommentsForMarketingCampaign();											
+		}
+	}
+
+	private void HandleTariffUpdatePressed(){
+	//	Server.handleMessageFromClient(new callbackStringArray(MessageType.getWaitingTariff));
+	//	callbackStringArray TariffTable = (callbackStringArray) getCallBackFromBuffer();		
+	//	GuiScreen.setTariffUpdateTable(new TableModel(TariffTable.getData(), TariffTable.getColHeaders()));
+	}
+
+	private void HandleCreateReportsPressed(){
+	
+	}
+	private void HandleActivateSaleCampaignPressed(){}
+	
+	private void HandleCustomerCharacterizationReport(){
+	//Server.handleMessageFromClient(new callbackStringArray(MessageType.getCommentsForMarketionCampaign));
+	//callbackStringArray TariffTable = (callbackStringArray) getCallBackFromBuffer();		
+	//GuiScreen.setTariffUpdateTable(new TableModel(TariffTable.getData(), TariffTable.getColHeaders()));
+	}
+	private void HandleCommentsForMarketingCampaign(){}
 }
+
