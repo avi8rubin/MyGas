b7fbcb0e9822496028d68eb20cc3bbb0ca59cb01
avi8rubin <avi8rubin@gmail.com>
2015-12-17 09:26:45 +0200

    Update Login_GUI.java
    הורדת ליטף ממסך login

--- MyGas/src/GUI/Login_GUI.java
+++ MyGas/src/GUI/Login_GUI.java
@@ -80,7 +80,7 @@ public class Login_GUI extends JFrame {
 		PasswordLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		PasswordLabel.setBounds(63, 388, 123, 35);
 		FirstLoginScreen.add(PasswordLabel);
-		UserNameTextField.setText("litaf");
+		UserNameTextField.setText("");
 		
 		//User Name Text Field
 		UserNameTextField.setToolTipText("Enter User Name");
