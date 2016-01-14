9c88748de0e00b4356a1a93a7d426e2acec2bbcc
zinoo <zinoo@OhadZino>
2016-01-14 14:35:38 +0200

    עדכון JAVADOC

--- MyGas/src/callback/callbackLostConnection.java
+++ MyGas/src/callback/callbackLostConnection.java
@@ -1,10 +1,11 @@
 package callback;
-
+/**
+ * This callback will send to client when connection to server lost
+ * @author Ohad
+ *
+ */
 public class callbackLostConnection extends CallBack {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 }
